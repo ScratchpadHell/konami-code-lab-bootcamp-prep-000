88b2c03187fbf9d364c9cc2c38e40783c3c16938
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-12 22:23:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(e) {
+ 
+const key = parseInt(e.detail||e.which);
 
+  if (key === code[index]) {
+    index++;
+  
+    if(index===code.length) {
+      alert("Fatality!");
+      index = 0;
+      }
+    } else {
+      index = 0;
+    }
 }
+
+const body = document.getElementById('body')
+ 
+main.addEventListener('click', function(init) {
+  alert("Fatality!")
+})
