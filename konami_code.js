a259a300c0fb824222ce0f6a01e4019baa71e08b
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-13 00:27:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init(e) {
  
  let index = 0
  document.body.addEventListener('keydown', function(e))
-const key = parseInt(e.detail||e.which);
+ const key = parseInt(e.detail||e.which);
 
   if (key === code[index]) {
     index++;
