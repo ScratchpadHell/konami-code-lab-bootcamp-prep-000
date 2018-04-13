c667e89db80bb62d4c68298cfe7c796d7091c05a
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-13 00:26:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init(e) {
  
  let index = 0
- document.body.addEventListener
+ document.body.addEventListener('keydown', )
 const key = parseInt(e.detail||e.which);
 
   if (key === code[index]) {
